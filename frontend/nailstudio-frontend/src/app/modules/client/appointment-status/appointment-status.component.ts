878f0ee7f5892e3de058a07a87697ff1aaa17a08
anastasijaac <anastasija.cvetanovic22@outlook.de>
2024-06-28 21:34:38 +0200

    kunden navbar improvement

--- frontend/nailstudio-frontend/src/app/modules/client/appointment-status/appointment-status.component.ts
+++ frontend/nailstudio-frontend/src/app/modules/client/appointment-status/appointment-status.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NavbarKundeComponent} from "../../../navigation/navbar-kunde/navbar-kunde.component";
 
 @Component({
   selector: 'app-appointment-status',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, NavbarKundeComponent],
   templateUrl: './appointment-status.component.html',
   styleUrls: ['./appointment-status.component.css']
 })
--- frontend/nailstudio-frontend/src/app/modules/client/feedback/feedback.component.ts
+++ frontend/nailstudio-frontend/src/app/modules/client/feedback/feedback.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NavbarKundeComponent} from "../../../navigation/navbar-kunde/navbar-kunde.component";
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, NavbarKundeComponent],
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css']
 })
